44f47289e704a08c85d604b2a0532b7d048fcecf
Logan Chantry <logan.chantry@ndus.edu>
2021-08-09 19:49:04 -0500

    Changes to improve gameplay

--- script.js
+++ script.js
@@ -1,67 +1,84 @@
 //ui
-import {TILE_STATUSES, createBoard, markTile, revealTile, checkLose, checkWin } from "./minesweeper.js";
+import { TILE_STATUSES, createBoard, markTile, revealTile, checkLose, checkWin } from "./minesweeper.js";
 
+let win, lose = false;
+let BOARD_SIZE = 10;
+let NUMBER_OF_MINES = 10;
+let board, boardElement, minesLeftText, messageText, markedTileCount;
+setupGame(BOARD_SIZE, NUMBER_OF_MINES);
 
-const BOARD_SIZE = 10;
-const NUMBER_OF_MINES = 10;
+let startGameButton = document.getElementById('startGameButton');
+startGameButton.addEventListener('click', () => {
+    startGame();
+})
 
-const board = createBoard(BOARD_SIZE,NUMBER_OF_MINES);
-const boardElement = document.querySelector('.board');
-const minesLeftText = document.querySelector("[data-mine-count]");
-const messageText = document.querySelector(".subtext");
+export function startGame() {
+    BOARD_SIZE = document.getElementById('numCells').value;
+    NUMBER_OF_MINES = document.getElementById('numMines').value;
+    board = [];
+    while (boardElement.firstChild) {
+        boardElement.removeChild(boardElement.firstChild);
+    }
+    boardElement.removeEventListener("click", stopProp, true);
+    boardElement.removeEventListener("contextmenu", stopProp, true);
+    win = false;
+    lose = false;
+    messageText.innerHTML = 'Mines Left: <span data-mine-count></span>';
+    markedTileCount = 0;
+    setupGame(BOARD_SIZE, NUMBER_OF_MINES)
+}
 
-board.forEach( row => {
-    row.forEach(tile => {
-        boardElement.append(tile.element);
-        tile.element.addEventListener('click', () => {
-            revealTile(board,tile);
-            checkGameEnd();
-        })
-        tile.element.addEventListener('contextmenu', e => {
-            e.preventDefault();
-            markTile(tile);
-            listMinesLeft();
+function setupGame(BOARD_SIZE, NUMBER_OF_MINES) {
+    board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
+    boardElement = document.querySelector('.board');
+    minesLeftText = document.querySelector("[data-mine-count]");
+    messageText = document.querySelector(".subtext");
+
+    board.forEach(row => {
+        row.forEach(tile => {
+            boardElement.append(tile.element);
+            tile.element.addEventListener('click', () => {
+                revealTile(board, tile);
+                checkGameEnd();
+            })
+            tile.element.addEventListener('contextmenu', e => {
+                e.preventDefault();
+                markTile(tile);
+                listMinesLeft();
+            })
         })
     })
-})
-boardElement.style.setProperty('--size',BOARD_SIZE);
-minesLeftText.textContent = NUMBER_OF_MINES;
-
-function listMinesLeft(){
-    const markedTileCount = board.reduce((count, row) => {
+    boardElement.style.setProperty('--size', BOARD_SIZE);
+    minesLeftText.textContent = NUMBER_OF_MINES;
+}
+function listMinesLeft() {
+    markedTileCount = board.reduce((count, row) => {
         return count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length;
     }, 0)
     minesLeftText.textContent = NUMBER_OF_MINES - markedTileCount;
 }
 
-function checkGameEnd(){
-    const win = checkWin(board);
-    const lose = checkLose(board);
+function checkGameEnd() {
+    win = checkWin(board);
+    lose = checkLose(board);
 
-    if(win || lose){
-        boardElement.addEventListener("click", stopProp, {capture: true});
-        boardElement.addEventListener("contextmenu", stopProp, {capture: true});
+    if (win || lose) {
+        boardElement.addEventListener("click", stopProp, { capture: true });
+        boardElement.addEventListener("contextmenu", stopProp, { capture: true });
     }
-    if(win){
+    if (win) {
         messageText.textContent = 'You Win';
     }
-    if(lose){
+    if (lose) {
         messageText.textContent = 'You Lose';
         board.forEach(row => {
             row.forEach(tile => {
-                if(tile.mine) revealTile(board, tile);
+                if (tile.mine) revealTile(board, tile);
             })
         })
     }
 }
 
-function stopProp(e){
+function stopProp(e) {
     e.stopImmediatePropagation();
 }
-
-//populate a board with tiles/mines
-//left click on tiles
-// a. reveal tiles
-//right click on tiles
-// a. mark tiles
-//check for win/loss
